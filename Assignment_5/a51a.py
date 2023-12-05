9ba9cfc0bd811f4b83d7ccc15550bfe9c55567b9
Kanakanajm <kanakanajm@outlook.com>
2023-12-05 14:07:53 +0100

    Added a5 1a help script

---
+++ Assignment_5/a51a.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+A = 1
+B = 100
+E = 0.002
+X0 = np.array([0.9, 1.12])
+
+def f(x):
+    return (A - x[0])**2 + B*(x[1] - x[0]**2)**2
+
+def gradient(x):
+    return np.array([
+        -2*(A - x[0]) - 4*B*x[0]*(x[1] - x[0]**2),
+        2*B*(x[1] - x[0]**2)
+    ])
+
+def about(x):
+    print("X:", x, "\tgrad:", gradient(x), "\tf:", f(x))
+
+X = X0
+NO_ITERATION = 3000
+GRAD_THRESHOLD = 0.01
+
+i = 0
+while(i < NO_ITERATION and np.linalg.norm(gradient(X)) > GRAD_THRESHOLD):
+    print("iteration", i+1)
+    about(X)
+
+    X = X - E * gradient(X)
+    i += 1
+
+print("result")
+about(X)
