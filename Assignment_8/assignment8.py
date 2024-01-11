35d2a292df5e95349b7ff461e3800d313c9a1e2b
Kanakanajm <kanakanajm@outlook.com>
2024-01-11 17:59:24 +0100

    Done A84

--- Assignment_8/assignment8.py
+++ Assignment_8/assignment8.py
@@ -2,6 +2,9 @@ from typing import Optional, Tuple
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
+import matplotlib.pyplot as plt
+import copy
+ 
 
 
 # 8.4.1
@@ -17,11 +20,18 @@ class FFNN(nn.Module):
         :params output_shape: Number of output units / classes
         """
         # Your code here
-        raise NotImplementedError
+        self.linear1 = nn.Linear(input_shape, hidden_units)
+        self.bnorm = nn.BatchNorm1d(hidden_units)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(hidden_units, output_shape)
 
     def forward(self, images):
         # Your code here
-        raise NotImplementedError
+        z = self.linear1(images)
+        z_normed = self.bnorm(z)
+        a = self.relu(z_normed)
+        out = self.linear2(a)
+        return out
 
 
 # 8.4.2
@@ -36,9 +46,37 @@ def train(model: nn.Module, optimizer: torch.optim.Optimizer, criterion, dataloa
 
     :return: A tuple containing the average loss and average accuracy over the dataset.
     """
-    
+    # switch the mode of the model
+    model.train()
     # Your code here
-    raise NotImplementedError
+    loss_sum = 0
+    correct = 0
+    total = 0
+    
+    for i, (images, labels) in enumerate(dataloader):
+        # device has to be the same as model
+        images.to("cuda" if torch.cuda.is_available() else "cpu")
+        labels.to("cuda" if torch.cuda.is_available() else "cpu")
+
+
+        images = images.view(-1, 28 * 28)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+
+        loss = criterion(outputs, torch.eye(10)[labels])
+        loss_sum += loss.item()
+
+        loss.backward()
+        optimizer.step()
+        total += labels.shape[0]
+        for bi in range(outputs.shape[0]):
+            if (torch.argmax(outputs[bi, :]) == labels[bi]):
+                correct += 1
+
+    return loss_sum / len(iter(dataloader)), correct / total
+
+    
 
 
 @torch.no_grad()
@@ -53,8 +91,30 @@ def validate(model: nn.Module, criterion, dataloader: DataLoader) -> Tuple[float
     :return: A tuple containing the average loss and average accuracy over the dataset.
     """
     # Your code here
-    raise NotImplementedError
+    # somehow has to switch mode for validation
+    model.eval()
 
+    loss_sum = 0
+    correct = 0
+    total = 0
+    
+    for i, (images, labels) in enumerate(dataloader):
+        images.to("cuda" if torch.cuda.is_available() else "cpu")
+        labels.to("cuda" if torch.cuda.is_available() else "cpu")
+
+
+        images = images.view(-1, 28 * 28)
+        outputs = model(images)
+
+        loss = criterion(outputs, torch.eye(10)[labels])
+        loss_sum += loss.item()
+
+        total += labels.shape[0]
+        for bi in range(outputs.shape[0]):
+            if (torch.argmax(outputs[bi, :]) == labels[bi]):
+                correct += 1
+
+    return loss_sum / len(iter(dataloader)), correct / total
 
 # 8.4.3
 def training_loop(
@@ -89,6 +149,46 @@ def training_loop(
     model.to("cuda" if torch.cuda.is_available() else "cpu")
 
     # Your code here
-    raise NotImplementedError
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay= weight_decay)
 
-    return model
+    train_history = []
+    val_history = []
+    best_accuracy = 0
+    best_model = None
+    bad_epoch = 0
+    for epoch in range(int(epochs)):
+        train_loss, train_accu = train(model, optimizer, criterion, train_loader)
+        val_loss, val_accu = validate(model, criterion, test_loader)
+        if (val_accu > best_accuracy): 
+            best_accuracy = val_accu
+            best_model = copy.deepcopy(model)
+
+        # check progress if early_stopping_patience is positive
+        if (early_stopping_patience > 0):
+            # comparing with the last validation accuracy
+            if (epoch > 0 and val_history[-1][1] >= val_accu):
+                bad_epoch += 1
+                if (bad_epoch > early_stopping_patience):
+                    print("Early stopped at", epoch)
+                    break
+            else:
+                bad_epoch = 0
+            
+        train_history.append((train_loss, train_accu))
+        val_history.append((val_loss, val_accu))
+        
+
+    
+
+
+    # plot stuff
+    plt.plot(range(len(train_history)), [train_history[i][0] for i in range(len(train_history))], label="train loss")
+    plt.plot(range(len(train_history)), [train_history[i][1] for i in range(len(train_history))], label="train accuracy")
+    plt.plot(range(len(val_history)), [val_history[i][0] for i in range(len(val_history))], label="validation loss")
+    plt.plot(range(len(val_history)), [val_history[i][1] for i in range(len(val_history))], label="validation accuracy")
+    plt.legend()
+    plt.show()
+
+    print("Best Accuracy:", best_accuracy)
+    return best_model
