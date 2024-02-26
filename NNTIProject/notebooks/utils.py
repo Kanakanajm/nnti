5e3248077127148de442c73eae9dbbf3bb4cc3ab
CamiloMartinezM <ca.martinezm2@uniandes.edu.co>
2024-02-26 20:46:44 +0100

    Task 1 update

--- NNTIProject/notebooks/utils.py
+++ NNTIProject/notebooks/utils.py
@@ -1,4 +1,13 @@
 from copy import deepcopy
+from pprint import pprint, pformat
+
+
+def left_indent(text, indent: str = "\t"):
+    return "".join([indent + l for l in text.splitlines(True)])
+
+
+def pprint_tab(obj, indent: str = "\t"):
+    print(left_indent(pformat(obj), indent))
 
 
 def get_available_objects(data):
